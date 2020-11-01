bd6000aa671b1de284de8b788b3c45ebad9919b8
Pablo Menz <p.menz.r@gmail.com>
2020-11-01 14:31:28 +0000

    debuuged useres controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
 
   post '/users/login' do
     @user = User.find_by(username: params[:username])
-    if (!@user.nil? && (@user.password == params[:password]))
+    #if (!@user.nil? && (@user.password == params[:password]))
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
     else
@@ -29,18 +30,21 @@ class UsersController < ApplicationController
     if
      (!@user.username.empty?) && (!@user.password.empty?)
      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
+     redirect "/users/#{@user.id}"
     elseif
-      User.find_by(username: params[:@user.username]).exist?
-      @signup_error = "Username already exist, please try again"
-      erb :"users/signup"
+     User.find_by(username: params[:@user.username]).exist?
+     @signup_error = "Username already exist, please try again"
+     erb :"users/signup"
     else
       @signup_error = "Please enter correct credentials"
       erb :"users/signup"
     end
   end
 
-
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :"users/show"
+  end
 
   post '/logout' do
     sessions.clear
