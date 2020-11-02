d509a6c7f6e2ca1ea2ec2539f2a1c2a86b7662a6
Pablo Menz <p.menz.r@gmail.com>
2020-11-02 18:25:23 +0000

    added session function, while logged in redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
 
+#CREATE
+
+  get '/users/signup' do
+    if logged_in?
+      redirect to '/users/:id'
+    end
+    erb :"/users/signup"
+  end
+
+  post '/users/signup' do
+    @user = User.create(params)
+    if
+     (!@user.username.empty?) && (!@user.password.empty?)
+     session[:user_id] = @user.id
+     redirect "/users/#{@user.id}"
+    elseif
+     User.find_by(username: params[:@user.username]).exist?
+     @signup_error = "Username already exist, please try again"
+     erb :"users/signup"
+    else
+     @signup_error = "Please enter correct credentials"
+     erb :"users/signup"
+    end
+  end
+
 #READ
 
   get '/welcome' do
@@ -16,45 +41,26 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
      else
-      @login_error = "Please enter correct credentials"
+      @login_error = @user
       erb :"/welcome"
      end
   end
 
   get '/users/:id' do
-    @user = User.find(params[:id])
-    erb :"users/show"
+    if logged_in?
+      @user = current_user
+      erb :"users/show"
+    end
   end
 
 #CREATE
-
-  get '/users/signup' do
+  get '/logout' do
     if logged_in?
-      redirect to '/users/:id'
-    end
-    erb :"/users/signup"
-  end
-
-  post '/users/signup' do
-    @user = User.create(params)
-    if
-     (!@user.username.empty?) && (!@user.password.empty?)
-     session[:user_id] = @user.id
-     redirect '/users/#{@user.id}'
-    elseif
-     User.find_by(username: params[:@user.username]).exist?
-     @signup_error = "Username already exist, please try again"
-     erb :"users/signup"
+      session.clear
+      redirect to '/'
     else
-     @signup_error = "Please enter correct credentials"
-     erb :"users/signup"
+      redirect to '/'
     end
-  end
-
-
-  post '/logout' do
-    sessions.clear
-    redirect '/'
   end
 
 end
