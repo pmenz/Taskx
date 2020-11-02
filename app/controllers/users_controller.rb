a2a765ce65288b1a6913115e4270d57c14e491b8
Pablo Menz <p.menz.r@gmail.com>
2020-11-02 20:28:15 +0000

    debuggin routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,65 +2,71 @@ class UsersController < ApplicationController
 
 #CREATE
 
-  get '/users/signup' do
+  get "/users/signup" do
     if logged_in?
-      redirect to '/users/:id'
+      redirect to "/users/:id"
     end
-    erb :"/users/signup"
+    erb :'/users/signup'
   end
 
-  post '/users/signup' do
-    @user = User.create(params)
-    if
-     (!@user.username.empty?) && (!@user.password.empty?)
-     session[:user_id] = @user.id
-     redirect "/users/#{@user.id}"
-    elseif
-     User.find_by(username: params[:@user.username]).exist?
-     @signup_error = "Username already exist, please try again"
-     erb :"users/signup"
+  post "/users/signup" do
+    if (params[:username]).empty? && (params[:password]).empty?
+      @signup_error = "Something went wrong! please try again"
+      erb :'/users/signup'
+    elsif !(User.find_by(username: params[:username]) == nil)
+      @signup_error = "Username already exist, please try again"
+      erb :'/users/signup'
     else
-     @signup_error = "Please enter correct credentials"
-     erb :"users/signup"
-    end
-  end
-
-#READ
-
-  get '/welcome' do
-    if logged_in?
-      redirect to '/users/:id'
-    else
-      erb :"/welcome"
-    end
-  end
-
-  post '/welcome' do
-     @user = User.find_by(username: params[:username])
-     if @user && @user.authenticate(params[:password])
+      @user = User.create(
+      username: params[:username],
+      password: params[:password]
+    )
       session[:user_id] = @user.id
+
+#     binding.pry
       redirect "/users/#{@user.id}"
-     else
-      @login_error = @user
-      erb :"/welcome"
-     end
+    end
   end
 
   get '/users/:id' do
+      binding.pry
+#    if logged_in?
+#     @user = User.find_by(params[:username])
+      erb :'users/show'
+#    end
+  end
+#READ
+
+  get "/welcome" do
     if logged_in?
-      @user = current_user
-      erb :"users/show"
+      redirect to "/users/:id"
+    else
+      erb :'/welcome'
     end
   end
+
+  post "/welcome" do
+     @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      current_user = @user
+      redirect to "/users/#{@user.id}"
+     else
+      @login_error = "Something went wrong! please try again"
+      erb :'/welcome'
+     end
+  end
+
+
 
 #CREATE
-  get '/logout' do
+  get "/logout" do
     if logged_in?
       session.clear
-      redirect to '/'
+      redirect to "/"
     else
-      redirect to '/'
+      redirect to "/"
     end
-  end
 
+  end
 end
